6bbb18bc1629b9a01bc5108d38968bd9eedd8058
Karl Belousov <belousovkarl@gmail.com>
2023-11-21 20:09:14 +0600

    хедер

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,6 @@
-/* в этот файл добавляет скрипты*/
+const headerNav = document.querySelector(".header__nav");
+const headerButton = document.querySelector(".header__button");
+
+headerButton.addEventListener("click", function () {
+  headerNav.classList.toggle("header__nav--opened");
+});
